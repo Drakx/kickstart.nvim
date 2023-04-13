b7f09fd0fc390f84d1e5840b65d7ae40487fc6f8
Kai Windle <kaiwindle@gmail.com>
2023-04-13 20:08:29 +0100

    sane defaults

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -1,13 +1,59 @@
 local options = {
-	relativenumber = true,                               -- Relative line numbers
-	undodir = os.getenv("HOME") .. "/.config/nvim/undodir", -- Undotree default location
-	cursorline = true,                                   -- Hightlight current line cursor is on
-	-- hlsearch = true,                                     -- Highlight all matches on previous search pattern
-	ignorecase = true,                                   -- Ignore case in search patterns
+	backup = false,                       -- creates a backup file
+	clipboard = "unnamedplus",            -- allows neovim to access the system clipboard
+	cmdheight = 2,                        -- more space in the neovim command line for displaying messages
+	completeopt = { "menuone", "noselect" }, -- mostly just for cmp
+	conceallevel = 0,                     -- so that `` is visible in markdown files
+	fileencoding = "utf-8",               -- the encoding written to a file
+	hlsearch = true,                      -- highlight all matches on previous search pattern
+	ignorecase = true,                    -- ignore case in search patterns
+	mouse = "a",                          -- allow the mouse to be used in neovim
+	pumheight = 10,                       -- pop up menu height
+	showmode = false,                     -- we don't need to see things like -- INSERT -- anymore
+	showtabline = 4,                      -- always show tabs
+	smartcase = true,                     -- smart case
+	smartindent = true,                   -- make indenting smarter again
+	splitbelow = true,                    -- force all horizontal splits to go below current window
+	splitright = true,                    -- force all vertical splits to go to the right of current window
+	swapfile = false,                     -- creates a swapfile
+	-- termguicolors = true,                    -- set term gui colors (most terminals support this)
+
+	timeoutlen = 500,       -- time to wait for a mapped sequence to complete (in milliseconds)
+	undofile = true,        -- enable persistent undo
+	updatetime = 300,       -- faster completion (4000ms default)
+	writebackup = false,    -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+	expandtab = false,      -- convert tabs to spaces
+	shiftwidth = 4,         -- the number of spaces inserted for each indentation
+	tabstop = 4,            -- insert 2 spaces for a tab
+	cursorline = true,      -- highlight the current line
+	number = true,          -- set numbered lines
+	relativenumber = true,  -- set relative numbered lines
+	numberwidth = 4,        -- set number column width to 2 {default 4}
+	signcolumn = "yes",     -- always show the sign column, otherwise it would shift the text each time
+	wrap = false,           -- display lines as one long line
+	scrolloff = 8,          -- is one of my fav
+	sidescrolloff = 8,
+	guifont = "monospace:h17", -- the font used in graphical neovim applications
+	background = "dark",
+	foldmethod = "syntax",  -- Folds
+	foldenable = true,
+	--set foldnestmax=10
+
+	--"set foldlevelstart=3
+
+	--set nofoldenable                         " Make sure folds are not folded when opening
+
+	--set foldlevel=2
+
+
+
+	--" autocmd FileType vim :set foldmethod=marker<CR>
+
 }
+
+
 
 for k, v in pairs(options) do
 	vim.opt[k] = v
 end
-
--- require('telescope').load_extension("projects")
+vim.cmd([[set iskeyword+=-]])
