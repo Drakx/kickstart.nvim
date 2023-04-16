dacc581bc9a94b7f6dff88556ec2018b5189af39
Kai Windle <kaiwindle@gmail.com>
2023-04-16 15:58:21 +0100

    comments for why this is here.

--- github-action.sh
+++ github-action.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+# Just a helper script for the github action
+# that will allow me to change my nvim config
+# and will push to a remote repo where I use
+# this repos nvim.
+
 OUTPUTDIR=output
 mkdir $OUTPUTDIR
 cp -R * $OUTPUTDIR/
