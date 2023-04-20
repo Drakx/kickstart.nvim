4b6f5c0fed73d5096cbf3c0ab070c66ae9760de9
Kai Windle <kaiwindle@gmail.com>
2023-04-20 21:44:49 +0100

    updates to go mapping

--- after/plugin/which-key.lua
+++ after/plugin/which-key.lua
@@ -19,8 +19,8 @@ local setup = {
 			text_objects = true, -- help for text objects triggered after entering an operator
 			windows = true, -- default bindings on <c-w>
 			nav = true, -- misc bindings to work with windows
-			z = true, -- bindings for folds, spelling and others prefixed with z
-			g = true, -- bindings for prefixed with g
+			z = true,   -- bindings for folds, spelling and others prefixed with z
+			g = true,   -- bindings for prefixed with g
 		},
 	},
 	-- add operators that will trigger motion and text object completion
@@ -50,15 +50,15 @@ local setup = {
 		winblend = 0,
 	},
 	layout = {
-		height = { min = 4, max = 25 },                                -- min and max height of the columns
-		width = { min = 20, max = 50 },                                -- min and max width of the columns
-		spacing = 3,                                                   -- spacing between columns
-		align = "left",                                                -- align columns left, center or right
+		height = { min = 4, max = 25 },                                        -- min and max height of the columns
+		width = { min = 20, max = 50 },                                        -- min and max width of the columns
+		spacing = 3,                                                           -- spacing between columns
+		align = "left",                                                        -- align columns left, center or right
 	},
-	ignore_missing = true,                                                 -- enable this to hide mappings for which you didn't specify a label
+	ignore_missing = true,                                                     -- enable this to hide mappings for which you didn't specify a label
 	hidden = { "<silent>", "<cmd>", "<Cmd>", "<cr>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
-	show_help = true,                                                      -- show help message on the command line when the popup is visible
-	triggers = "auto",                                                     -- automatically setup triggers
+	show_help = true,                                                          -- show help message on the command line when the popup is visible
+	triggers = "auto",                                                         -- automatically setup triggers
 	-- triggers = {"<leader>"} -- or specify a list manually
 	triggers_blacklist = {
 		-- list of mode / prefixes that should never be hooked by WhichKey
@@ -70,7 +70,7 @@ local setup = {
 }
 
 local opts = {
-	mode = "n", -- NORMAL mode
+	mode = "n",  -- NORMAL mode
 	prefix = "<leader>",
 	buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
 	silent = true, -- use `silent` when creating keymaps
@@ -159,11 +159,12 @@ local mappings = {
 			t = {
 				name = "Test",
 				a = { "<cmd>GoTest ./...<cr>", "go test ./..." },
-				s = { "<cmd>GoTestFunc<cr>", "go test -s [current test]" },
+				f = { "<cmd>GoTestFunc<cr>", "go test -s [current test]" },
 				d = {
 					"<cmd>call vimspector#LaunchWithSettings( #{ configuration: 'single test', TestName: go#util#TestName() } )<cr>",
 					"Debug current test",
 				},
+				x = { "<cmd>GoTermClose<cr>", "[C]lose Test Term" },
 			},
 		},
 		i = { "<cmd>LspInfo<cr>", "[I]nfo" },
