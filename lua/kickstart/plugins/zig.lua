d0681173878be1afdd3ec0fbb10c6358c5dee740
Kai Windle <kaiwindle@gmail.com>
2023-05-31 12:18:26 +0100

    Update zig.lua

--- lua/kickstart/plugins/zig.lua
+++ lua/kickstart/plugins/zig.lua
@@ -1,3 +1,13 @@
+local on_attach = function(_, bufnr)
+    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+    require('completion').on_attach()
+end
+-- Set completeopt to have a better completion experience
+vim.o.completeopt = "menuone,noinsert,noselect"
+
+-- Enable completions as you type
+vim.g.completion_enable_auto_popup=1
+
 return {
   "ziglang/zig.vim",
   version = "*",
@@ -6,16 +16,9 @@ return {
     "nvim-lua/completion-nvim",
   },
   config = function()
-    local on_attach = function(_, bufnr)
-        vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-        require('completion').on_attach()
-    end
     require("zls").setup {
       on_attach = on_attach,
-    },
-    -- Set completeopt to have a better completion experience
-    set completeopt=menuone,noinsert,noselect,
-    -- Enable completions as you type
-    let g:completion_enable_auto_popup = 1,
+    }
   end,
 }
+
